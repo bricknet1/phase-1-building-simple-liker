60a8108c6105470b72893dbc7c79e9f49e35e9f5
Nick Johnson <nlj222@gmail.com>
2023-01-17 07:56:08 -0800

    lab complete

--- main.js
+++ main.js
@@ -6,6 +6,27 @@ const FULL_HEART = '♥'
 
 
 
+const glyphs = document.querySelectorAll(".like-glyph")
+const errMessage = document.querySelector("#modal")
+
+function heartClick(event) {
+  const heart = event.target;
+  mimicServerCall()
+    .then(() => {
+      if (heart.textContent === EMPTY_HEART) {heart.textContent = FULL_HEART
+      } else if (heart.textContent === FULL_HEART) {heart.textContent = EMPTY_HEART};
+    })
+    .catch((error) => {errMessage.className = "";
+    errMessage.textContent = error;
+    setTimeout(() => errMessage.className = "hidden", 3000);
+    });
+}
+
+for (const glyph of glyphs) {
+  glyph.addEventListener("click", heartClick);
+}
+
+
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -15,6 +36,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
+      console.log(isRandomFailure)
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
